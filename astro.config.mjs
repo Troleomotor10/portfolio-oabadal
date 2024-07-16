d3552d03e3334f60794c4ed8d3696c27aa964462
Troleomotor10 <oriol@visiorware.com>
2024-07-16 18:16:17 +0200

    update packages + add cloudflare connector

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://www.oabadal.dev/',
+  output: "server",
+  adapter: cloudflare()
+});
