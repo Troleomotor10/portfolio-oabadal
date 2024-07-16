7cd96e4e5158b1b306c71f65cd9b4347c80aca67
Troleomotor10 <oriol@visiorware.com>
2024-07-16 18:53:36 +0200

    update packages + remove unused import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
 import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://www.oabadal.dev/',
-  output: "server",
+  site: "https://www.oabadal.dev/",
+  output: "hybrid",
   adapter: cloudflare()
 });
